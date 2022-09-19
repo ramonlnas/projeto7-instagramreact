9df2864174b422db808a057f765fa899fa452a0b
ramonlnas <lopessramon0@gmail.com>
2022-09-19 16:19:36 -0300

    Botão curtir pronto e alterar numero de likes do post

--- src/Posts.js
+++ src/Posts.js
@@ -1,5 +1,30 @@
+import react from "react";
+
 function Post(props) {
+  const [like, setLike] = react.useState(false)
+  const [salva,setSalva] = react.useState(false)
+  const [count, setCount] = react.useState(props.pessoasCurtida)
+  function likePost () {
+   if (like === false){
+    setLike(true)
+    setCount((Number(count) + 0.001).toFixed(3))
+
+   }else{
+    setLike(false)
+    setCount((Number(count) - 0.001).toFixed(3))
+   }
+  }
+  function salvaPost () {
+    if(salva === false){
+      setSalva(true)
+    }else{
+      setSalva(false)
+    }
+  }
+  
+
   return (
+    
     <div class="post">
       <div class="topo">
         <div class="usuario">
@@ -12,18 +37,18 @@ function Post(props) {
       </div>
 
       <div class="conteudo">
-        <img src={props.imagem} />
+        <img class={(like === false ? "" : "colorido") } onClick={likePost} name={(like === false ? "heart-outline" : "heart")} src={props.imagem} />
       </div>
 
       <div class="fundo">
         <div class="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon class={(like === false ? "" : "colorido") } onClick={likePost} name={(like === false ? "heart-outline" : "heart")} ></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={salvaPost} name={salva === false ? "bookmark-outline" : "bookmark"}></ion-icon>
           </div>
         </div>
 
@@ -32,7 +57,7 @@ function Post(props) {
           <div class="texto">
             Curtido por 
              <strong> {props.usuarioCurtida}</strong> e
-            <strong> outras {props.pessoasCurtida} pessoas</strong>
+            <strong> outras {count} pessoas</strong>
           </div>
         </div>
       </div>
@@ -48,7 +73,7 @@ export default function Posts() {
     imagem="assets/img/gato-telefone.svg"
     pagCurtida = "assets/img/respondeai.svg"
     usuarioCurtida = "respondeai"
-    pessoasCurtida = "101523"
+    pessoasCurtida = {101.523}
 
 
   />,
@@ -58,7 +83,7 @@ export default function Posts() {
     imagem="assets/img/dog.svg"
     pagCurtida="assets/img/adorable_animals.svg"
     usuarioCurtida="adorable_animals"
-    pessoasCurtida="200541"
+    pessoasCurtida={99.159}
   />
 ]
   return (
